3df7d6c3ea3d44815c1051d3c9ca77039473d6e9
Jiordi Kengne <jiordikengne@gmail.com>
2024-03-23 12:18:13 +0100

    Mon projet de ECommerce

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -155,7 +155,7 @@ class OrderResource extends Resource
                                     $total += $get("items.$key.total_amount");
                                 }
                                 $set('grand_total', $total);
-                                return Number::currency($total, 'INR');
+                                return Number::currency($total, 'XAF');
                             }),
 
                         Forms\Components\Hidden::make('grand_total')
@@ -177,7 +177,7 @@ class OrderResource extends Resource
                 Tables\Columns\TextColumn::make('grand_total')
                     ->numeric()
                     ->sortable()
-                    ->money('INR'),
+                    ->money('XAF'),
                 Tables\Columns\TextColumn::make('payment_method')
                     ->searchable()
                     ->sortable(),
