21895d8781c3a170a3aee67a9f5fbe8998284be8
Jiordi Kengne <jiordikengne@gmail.com>
2024-03-23 12:18:13 +0100

    Mon projet de ECommerce

--- app/Livewire/Auth/ForgotPasswordPage.php
+++ app/Livewire/Auth/ForgotPasswordPage.php
@@ -2,10 +2,27 @@
 
 namespace App\Livewire\Auth;
 
+use Illuminate\Support\Facades\Password;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Forgot password')]
 class ForgotPasswordPage extends Component
 {
+
+    public string $email;
+
+    public function save()
+    {
+       $this->validate([
+           'email' => 'required|email|exists:users,email|max:255'
+       ]) ;
+       $status= Password::sendResetLink(['email'=> $this->email]);
+       if($status === Password::RESET_LINK_SENT){
+           session()->flash('success', 'We have e-mailed your password reset link!');
+           $this->email='';
+       }
+    }
     public function render()
     {
         return view('livewire.auth.forgot-password-page');
